e812c5828334b6309776da6f25f695051c1be093
BenoitAdam94 <adam050986@yahoo.fr>
2026-01-21 10:32:45 +0100

    first commit

---
+++ plugs/drkPvr/Renderer_if.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "drkPvr.h"
+#include "ta.h"
+//#include "TexCache.h"
+
+extern u32 VertexCount;
+extern u32 FrameCount;
+
+#include "gsRend.h"
+#include "gxRend.h"
+#include "nullRend.h"
+#include "glesRend.h"
+#include "softRend.h"
+
