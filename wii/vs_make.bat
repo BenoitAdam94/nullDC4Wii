d92e3e23e503267e7d5e50ef6d06663d095c190d
BenoitAdam94 <adam050986@yahoo.fr>
2026-01-26 11:54:29 +0100

    update

--- wii/vs_make.bat
+++ wii/vs_make.bat
@@ -1,1 +1,5 @@
-make 2>&1 >>compilation.log | sed -e "s/\([^:]*\):\([0-9][0-9]*\)\(.*\)/\1(\2) \3/"
+:: If you wanna log the compilation, do this. Beware, in will not do the dol/elf
+:: make 2>&1 >> compilation.log | sed -e "s/\([^:]*\):\([0-9][0-9]*\)\(.*\)/\1(\2) \3/"
+
+clear
+make 2>&1 | sed -e "s/\([^:]*\):\([0-9][0-9]*\)\(.*\)/\1(\2) \3/"
