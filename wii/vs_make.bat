c1b7ad97f5e71f727cc1c92dc14a88d580742caa
BenoitAdam94 <adam050986@yahoo.fr>
2026-02-05 12:36:50 +0100

    update

--- wii/vs_make.bat
+++ wii/vs_make.bat
@@ -1,5 +1,8 @@
 :: If you wanna log the compilation, do this. Beware, in will not do the dol/elf
 :: make 2>&1 >> compilation.log | sed -e "s/\([^:]*\):\([0-9][0-9]*\)\(.*\)/\1(\2) \3/"
+:: -j3 determine your CPU core. I personnaly use 3 core on my 4 core to let me a little bit of power for other apps
+:: -j$(nproc) if you don't know how many core your have
+:: clear : clear cmd screen
 
 clear
-make 2>&1 | sed -e "s/\([^:]*\):\([0-9][0-9]*\)\(.*\)/\1(\2) \3/"
+make 2>&1 -j3 | sed -e "s/\([^:]*\):\([0-9][0-9]*\)\(.*\)/\1(\2) \3/"
