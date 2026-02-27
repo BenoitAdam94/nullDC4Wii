fd5a6491c215c09ac873256aff28f64b468709ab
BenoitAdam94 <adam050986@yahoo.fr>
2026-02-27 23:32:10 +0100

    dmac: add null/bounds guards in Ch2St, refactor Init boilerplate
    - Guard GetMemPtr() results against null before TAWrite/WriteMemBlock
    - Skip Ch2 transfer early if SB_C2DLEN is zero
    - Extract destination range checks into inline helpers
    - Replace repetitive dmac_Init() register setup with macros
    - Use loop in dmac_Reset() over 4 channels
    - Add bit-field docs in dmac.h for SH4 register reference

--- dc/sh4/dmac.cpp
+++ dc/sh4/dmac.cpp
@@ -1,4 +1,6 @@
-//duh .. this will be dmac
+// dmac.cpp - SH4 DMAC (Direct Memory Access Controller) emulation
+// Handles Ch2-DMA (PVR/GPU feed) and register I/O mapping for all 4 channels.
+
 #include "types.h"
 #include "dc/mem/sh4_internal_reg.h"
 #include "dc/mem/sb.h"
@@ -9,294 +11,257 @@
 #include "dc/asic/asic.h"
 #include "plugins/plugin_manager.h"
 
-u32 DMAC_SAR[4];
-u32 DMAC_DAR[4];
-u32 DMAC_DMATCR[4];
-DMAC_CHCR_type DMAC_CHCR[4];
+// ---------------------------------------------------------------------------
+// DMAC register state for all 4 channels
+// ---------------------------------------------------------------------------
+u32              DMAC_SAR[4];
+u32              DMAC_DAR[4];
+u32              DMAC_DMATCR[4];   // only lower 24 bits are valid per channel
+DMAC_CHCR_type   DMAC_CHCR[4];
+DMAC_DMAOR_type  DMAC_DMAOR;
 
-DMAC_DMAOR_type DMAC_DMAOR;
+// ---------------------------------------------------------------------------
+// Address range helpers (makes intent explicit, avoids magic numbers)
+// ---------------------------------------------------------------------------
+static inline bool isDstTA(u32 dst)         { return dst >= 0x10000000u && dst <= 0x10FFFFFFu; }
+static inline bool isDstVRAM_LM0(u32 dst)  { return dst >= 0x11000000u && dst <= 0x11FFFFE0u; }
+static inline bool isDstVRAM_LM1(u32 dst)  { return dst >= 0x13000000u && dst <= 0x13FFFFE0u; }
 
+// ---------------------------------------------------------------------------
+// DMAC_Ch2St - Channel 2 DMA: feeds the PowerVR TA / VRAM
+// Called when SB_C2DST is written to trigger a Ch2 transfer.
+// ---------------------------------------------------------------------------
 void DMAC_Ch2St()
 {
-	u32 chcr	= DMAC_CHCR[2].full,
-		dmaor	= DMAC_DMAOR.full,
-		dmatcr	= DMAC_DMATCR[2];
+	const u32 dmaor  = DMAC_DMAOR.full;
+	const u32 dst    = SB_C2DSTAT;
+	u32       src    = DMAC_SAR[2];
+	u32       len    = SB_C2DLEN;
 
-	u32	src		= DMAC_SAR[2],
-		dst		= SB_C2DSTAT,
-		len		= SB_C2DLEN ;
-
-	if(0x8201 != (dmaor &DMAOR_MASK)) {
-		printf("\n!\tDMAC: DMAOR has invalid settings (%X) !\n", dmaor);
-		return;
-	}
-	if( len & 0x1F ) {
-		printf("\n!\tDMAC: SB_C2DLEN has invalid size (%X) !\n", len);
+	// Validate DMAOR: must have DME set and DDT mode, no error flags
+	if ((dmaor & DMAOR_MASK) != 0x8201u)
+	{
+		printf("DMAC: Ch2 abort - DMAOR invalid (0x%08X)\n", dmaor);
 		return;
 	}
 
-//	printf(">>\tDMAC: Ch2 DMA SRC=%X DST=%X LEN=%X\n", src, dst, len );
-
-	// Direct DList DMA (Ch2)
-
-			// Texture DMA 
-	if( (dst >= 0x10000000) && (dst <= 0x10FFFFFF) )
+	// SB_C2DLEN must be a multiple of 32 bytes
+	if (len & 0x1Fu)
 	{
-		u32 p_addr=src & RAM_MASK;
-		//GetMemPtr perhaps ? it's not good to use teh mem arrays directly 
-		while(len)
+		printf("DMAC: Ch2 abort - SB_C2DLEN not 32-byte aligned (0x%X)\n", len);
+		return;
+	}
+
+	// Guard against zero-length transfer
+	if (len == 0)
+	{
+		printf("DMAC: Ch2 abort - SB_C2DLEN is zero\n");
+		return;
+	}
+
+	//printf("DMAC: Ch2 DMA  SRC=0x%08X  DST=0x%08X  LEN=0x%X\n", src, dst, len);
+
+	// --- Transfer to TA (display list) address space ---
+	if (isDstTA(dst))
+	{
+		while (len)
 		{
-			if ((p_addr+len)>RAM_SIZE)
+			const u32 p_addr  = src & RAM_MASK;
+			const u32 avail   = RAM_SIZE - p_addr;
+
+			if (avail < len)
 			{
-				u32 *sys_buf=(u32 *)GetMemPtr(src,len);//(&mem_b[src&RAM_MASK]);
-				u32 new_len=RAM_SIZE-p_addr;
-				TAWrite(dst,sys_buf,(new_len/32));
-				len-=new_len;
-				src+=new_len;
-				//dst+=new_len;
+				// Transfer up to the RAM wrap boundary, then loop
+				u32 *sys_buf = (u32 *)GetMemPtr(src, avail);
+				if (sys_buf)
+					TAWrite(dst, sys_buf, avail / 32);
+				len -= avail;
+				src += avail;
 			}
 			else
 			{
-				u32 *sys_buf=(u32 *)GetMemPtr(src,len);//(&mem_b[src&RAM_MASK]);
-				TAWrite(dst,sys_buf,(len/32));
-				src+=len;
+				u32 *sys_buf = (u32 *)GetMemPtr(src, len);
+				if (sys_buf)
+					TAWrite(dst, sys_buf, len / 32);
+				src += len;
 				break;
 			}
 		}
-		//libPvr_TADma(dst,sys_buf,(len/32));
 	}
-	else	//	If SB_C2DSTAT reg is inrange from 0x11000000 to 0x11FFFFE0,	 set 1 in SB_LMMODE0 reg.
-	if( (dst >= 0x11000000) && (dst <= 0x11FFFFE0) )
+	// --- Transfer to VRAM via LMMODE0 (0x11xxxxxx -> VRAM bank 0) ---
+	else if (isDstVRAM_LM0(dst))
 	{
-		//printf(">>\tDMAC: TEX LNMODE0 Ch2 DMA SRC=%X DST=%X LEN=%X | LN(%X::%X)\n", src, dst, len, *pSB_LMMODE0, *pSB_LMMODE1 );
+		u32 vram_dst = (dst & 0x00FFFFFFu) | 0xA4000000u;
 
-		dst=(dst&0xFFFFFF) |0xa4000000;
-		/*WriteMemBlock_nommu_ptr(dst,(u32*)GetMemPtr(src,len),len);
-		src+=len;*/
-		u32 p_addr=src & RAM_MASK;
-		while(len)
+		while (len)
 		{
-			if ((p_addr+len)>RAM_SIZE)
+			const u32 p_addr  = src & RAM_MASK;
+			const u32 avail   = RAM_SIZE - p_addr;
+
+			if (avail < len)
 			{
-				u32 *sys_buf=(u32 *)GetMemPtr(src,len);//(&mem_b[src&RAM_MASK]);
-				u32 new_len=RAM_SIZE-p_addr;
-				WriteMemBlock_nommu_ptr(dst,sys_buf,new_len);
-				len-=new_len;
-				src+=new_len;
-				dst+=new_len;
+				u32 *sys_buf = (u32 *)GetMemPtr(src, avail);
+				if (sys_buf)
+					WriteMemBlock_nommu_ptr(vram_dst, sys_buf, avail);
+				len      -= avail;
+				src      += avail;
+				vram_dst += avail;
 			}
 			else
 			{
-				u32 *sys_buf=(u32 *)GetMemPtr(src,len);//(&mem_b[src&RAM_MASK]);
-				WriteMemBlock_nommu_ptr(dst,sys_buf,len);
-				src+=len;
+				u32 *sys_buf = (u32 *)GetMemPtr(src, len);
+				if (sys_buf)
+					WriteMemBlock_nommu_ptr(vram_dst, sys_buf, len);
+				src += len;
 				break;
 			}
 		}
-	//	*pSB_LMMODE0 = 1;			// this prob was done by system already
-	//	WriteMem(SB_LMMODE1, 0, 4);	// should this be done ?
 	}
-	else	//	If SB_C2DSTAT reg is inrange from 0x13000000 to 0x13FFFFE0,	 set 1 in SB_LMMODE1 reg.
-	if( (dst >= 0x13000000) && (dst <= 0x13FFFFE0) )
+	// --- Transfer to VRAM via LMMODE1 (0x13xxxxxx -> VRAM bank 1) ---
+	else if (isDstVRAM_LM1(dst))
 	{
-		printf(".\tPVR DList DMA LNMODE1\n\n");
-		src+=len;
-	//	*pSB_LMMODE1 = 1;			// this prob was done by system already
-	//	WriteMem(SB_LMMODE0, 0, 4);	// should this be done ?
+		// LMMODE1 path not yet fully implemented; advance src to keep state consistent
+		printf("DMAC: Ch2 LMMODE1 transfer (stub) DST=0x%08X LEN=0x%X\n", dst, len);
+		src += len;
 	}
-	else 
-	{ 
-		printf("\n!\tDMAC: SB_C2DSTAT has invalid address (%X) !\n", dst); 
-		src+=len;
+	else
+	{
+		printf("DMAC: Ch2 invalid SB_C2DSTAT address (0x%08X)\n", dst);
+		src += len;
 	}
 
+	// --- Update registers to reflect completed transfer ---
+	DMAC_SAR[2]       = src;
+	DMAC_CHCR[2].full &= ~0x1u;   // clear DE (channel enable)
+	DMAC_DMATCR[2]    = 0x00000000u;
 
-	// Setup some of the regs so it thinks we've finished DMA
+	SB_C2DST          = 0x00000000u;
+	SB_C2DLEN         = 0x00000000u;
+	SB_C2DSTAT        = src;
 
-	DMAC_SAR[2] = (src);
-	DMAC_CHCR[2].full &= 0xFFFFFFFE;
-	DMAC_DMATCR[2] = 0x00000000;
-
-	SB_C2DST = 0x00000000;
-	SB_C2DLEN = 0x00000000;
-	SB_C2DSTAT = (src );
-
-	// The DMA end interrupt flag (SB_ISTNRM - bit 19: DTDE2INT) is set to "1."
-	//-> fixed , holly_PVR_DMA is for diferent use now (fixed the interrupts enum too)
+	// Raise Ch2-DMA end interrupt (ISTNRM bit 19: DTDE2INT)
 	asic_RaiseInterrupt(holly_CH2_DMA);
 }
 
-//on demand data transfer
-//ch0/on demand data transfer request
-void dmac_ddt_ch0_ddt(u32 src,u32 dst,u32 count)
+// ---------------------------------------------------------------------------
+// On-demand data transfer stubs (not yet implemented)
+// ---------------------------------------------------------------------------
+void dmac_ddt_ch0_ddt(u32 src, u32 dst, u32 count)
 {
-	
+	// TODO: Ch0 on-demand data transfer
+	(void)src; (void)dst; (void)count;
 }
-//ch2/direct data transfer request
-void dmac_ddt_ch2_direct(u32 dst,u32 count)
+
+void dmac_ddt_ch2_direct(u32 dst, u32 count)
 {
+	// TODO: Ch2 direct data transfer (DDT mode)
+	(void)dst; (void)count;
 }
-//transfer 22kb chunks (or less) [704 x 32] (22528)
+
+// ---------------------------------------------------------------------------
+// UpdateDMA - cycle-accurate DMA tick (not yet implemented)
+// ---------------------------------------------------------------------------
 void UpdateDMA()
 {
-	/*if (DMAC_DMAOR.AE==1 || DMAC_DMAOR.DME==0)
-		return;//DMA disabled
-
-	//DMAC _must_ be on DDT mode
-	verify(DMAC_DMAOR.DDT==1);
-
-	for (int ch=0;ch<4;ch++)
-	{
-		if (DMAC_CHCR[ch].DE==1 && DMAC_CHCR[ch].TE==0)
-		{
-			verify(DMAC_CHCR[ch].RS<0x8);
-			verify(DMAC_CHCR[ch].RS<0x4);
-		}
-	}*/
+	// TODO: implement cycle-accurate / timesliced DMA
+	// Requirements:
+	//   - DMAOR.DME must be set and DMAOR.AE must be clear
+	//   - DMAOR.DDT must be 1 for DDT mode
+	//   - For each active channel (CHCR[ch].DE==1 && CHCR[ch].TE==0):
+	//       RS field must be in range 0x0-0x7 for normal mode, 0x0-0x3 for some modes
+	//   - Transfer in 22 KB chunks (704 x 32 bytes = 22528 bytes)
 }
+
+// ---------------------------------------------------------------------------
+// CHCR / DMAOR write handlers (used as callbacks in the register map)
+// ---------------------------------------------------------------------------
 template<u32 ch>
-void WriteCHCR(u32 data)
+static void WriteCHCR(u32 data)
 {
-	DMAC_CHCR[ch].full=data;
-	//printf("Write to CHCR%d = 0x%X\n",ch,data);
+	DMAC_CHCR[ch].full = data;
+	//printf("DMAC: Write CHCR%u = 0x%08X\n", ch, data);
 }
-void WriteDMAOR(u32 data)
+
+static void WriteDMAOR(u32 data)
 {
-	DMAC_DMAOR.full=data;
-	//printf("Write to DMAOR = 0x%X\n",data);
+	DMAC_DMAOR.full = data;
+	//printf("DMAC: Write DMAOR = 0x%08X\n", data);
 }
-//Init term res
+
+// ---------------------------------------------------------------------------
+// dmac_Init - register all DMAC MMIO registers into the SH4 register map
+// Registers span 0xFFA00000 (P4) / 0x1FA00000 (physical), 4-byte aligned.
+// ---------------------------------------------------------------------------
+
+// Convenience macro to reduce the repetitive register-map boilerplate
+#define DMAC_REG_RW(addr, ptr) \
+	do { \
+		DMAC[((addr) & 0xFF) >> 2].flags         = REG_32BIT_READWRITE | REG_READ_DATA | REG_WRITE_DATA; \
+		DMAC[((addr) & 0xFF) >> 2].readFunction  = 0; \
+		DMAC[((addr) & 0xFF) >> 2].writeFunction = 0; \
+		DMAC[((addr) & 0xFF) >> 2].data32        = (ptr); \
+	} while(0)
+
+#define DMAC_REG_CHCR(addr, ptr, fn) \
+	do { \
+		DMAC[((addr) & 0xFF) >> 2].flags         = REG_32BIT_READWRITE | REG_READ_DATA; \
+		DMAC[((addr) & 0xFF) >> 2].readFunction  = 0; \
+		DMAC[((addr) & 0xFF) >> 2].writeFunction = (fn); \
+		DMAC[((addr) & 0xFF) >> 2].data32        = (ptr); \
+	} while(0)
+
 void dmac_Init()
 {
-	//DMAC SAR0 0xFFA00000 0x1FA00000 32 Undefined Undefined Held Held Bclk
-	DMAC[(DMAC_SAR0_addr&0xFF)>>2].flags=REG_32BIT_READWRITE | REG_READ_DATA | REG_WRITE_DATA;
-	DMAC[(DMAC_SAR0_addr&0xFF)>>2].readFunction=0;
-	DMAC[(DMAC_SAR0_addr&0xFF)>>2].writeFunction=0;
-	DMAC[(DMAC_SAR0_addr&0xFF)>>2].data32=&DMAC_SAR[0];
+	// Channel 0
+	DMAC_REG_RW  (DMAC_SAR0_addr,   &DMAC_SAR[0]);
+	DMAC_REG_RW  (DMAC_DAR0_addr,   &DMAC_DAR[0]);
+	DMAC_REG_RW  (DMAC_DMATCR0_addr,&DMAC_DMATCR[0]);
+	DMAC_REG_CHCR(DMAC_CHCR0_addr,  &DMAC_CHCR[0].full, WriteCHCR<0>);
 
-	//DMAC DAR0 0xFFA00004 0x1FA00004 32 Undefined Undefined Held Held Bclk
-	DMAC[(DMAC_DAR0_addr&0xFF)>>2].flags=REG_32BIT_READWRITE | REG_READ_DATA | REG_WRITE_DATA;
-	DMAC[(DMAC_DAR0_addr&0xFF)>>2].readFunction=0;
-	DMAC[(DMAC_DAR0_addr&0xFF)>>2].writeFunction=0;
-	DMAC[(DMAC_DAR0_addr&0xFF)>>2].data32=&DMAC_DAR[0];
+	// Channel 1
+	DMAC_REG_RW  (DMAC_SAR1_addr,   &DMAC_SAR[1]);
+	DMAC_REG_RW  (DMAC_DAR1_addr,   &DMAC_DAR[1]);
+	DMAC_REG_RW  (DMAC_DMATCR1_addr,&DMAC_DMATCR[1]);
+	DMAC_REG_CHCR(DMAC_CHCR1_addr,  &DMAC_CHCR[1].full, WriteCHCR<1>);
 
-	//DMAC DMATCR0 0xFFA00008 0x1FA00008 32 Undefined Undefined Held Held Bclk
-	DMAC[(DMAC_DMATCR0_addr&0xFF)>>2].flags=REG_32BIT_READWRITE | REG_READ_DATA | REG_WRITE_DATA;
-	DMAC[(DMAC_DMATCR0_addr&0xFF)>>2].readFunction=0;
-	DMAC[(DMAC_DMATCR0_addr&0xFF)>>2].writeFunction=0;
-	DMAC[(DMAC_DMATCR0_addr&0xFF)>>2].data32=&DMAC_DMATCR[0];
+	// Channel 2
+	DMAC_REG_RW  (DMAC_SAR2_addr,   &DMAC_SAR[2]);
+	DMAC_REG_RW  (DMAC_DAR2_addr,   &DMAC_DAR[2]);
+	DMAC_REG_RW  (DMAC_DMATCR2_addr,&DMAC_DMATCR[2]);
+	DMAC_REG_CHCR(DMAC_CHCR2_addr,  &DMAC_CHCR[2].full, WriteCHCR<2>);
 
-	//DMAC CHCR0 0xFFA0000C 0x1FA0000C 32 0x00000000 0x00000000 Held Held Bclk
-	DMAC[(DMAC_CHCR0_addr&0xFF)>>2].flags=REG_32BIT_READWRITE | REG_READ_DATA;
-	DMAC[(DMAC_CHCR0_addr&0xFF)>>2].readFunction=0;
-	DMAC[(DMAC_CHCR0_addr&0xFF)>>2].writeFunction=WriteCHCR<0>;
-	DMAC[(DMAC_CHCR0_addr&0xFF)>>2].data32=&DMAC_CHCR[0].full;
+	// Channel 3
+	DMAC_REG_RW  (DMAC_SAR3_addr,   &DMAC_SAR[3]);
+	DMAC_REG_RW  (DMAC_DAR3_addr,   &DMAC_DAR[3]);
+	DMAC_REG_RW  (DMAC_DMATCR3_addr,&DMAC_DMATCR[3]);
+	DMAC_REG_CHCR(DMAC_CHCR3_addr,  &DMAC_CHCR[3].full, WriteCHCR<3>);
 
-	//DMAC SAR1 0xFFA00010 0x1FA00010 32 Undefined Undefined Held Held Bclk
-	DMAC[(DMAC_SAR1_addr&0xFF)>>2].flags=REG_32BIT_READWRITE | REG_READ_DATA | REG_WRITE_DATA;
-	DMAC[(DMAC_SAR1_addr&0xFF)>>2].readFunction=0;
-	DMAC[(DMAC_SAR1_addr&0xFF)>>2].writeFunction=0;
-	DMAC[(DMAC_SAR1_addr&0xFF)>>2].data32=&DMAC_SAR[1];
-
-	//DMAC DAR1 0xFFA00014 0x1FA00014 32 Undefined Undefined Held Held Bclk
-	DMAC[(DMAC_DAR1_addr&0xFF)>>2].flags=REG_32BIT_READWRITE | REG_READ_DATA | REG_WRITE_DATA;
-	DMAC[(DMAC_DAR1_addr&0xFF)>>2].readFunction=0;
-	DMAC[(DMAC_DAR1_addr&0xFF)>>2].writeFunction=0;
-	DMAC[(DMAC_DAR1_addr&0xFF)>>2].data32=&DMAC_DAR[1];
-
-	//DMAC DMATCR1 0xFFA00018 0x1FA00018 32 Undefined Undefined Held Held Bclk
-	DMAC[(DMAC_DMATCR1_addr&0xFF)>>2].flags=REG_32BIT_READWRITE | REG_READ_DATA | REG_WRITE_DATA;
-	DMAC[(DMAC_DMATCR1_addr&0xFF)>>2].readFunction=0;
-	DMAC[(DMAC_DMATCR1_addr&0xFF)>>2].writeFunction=0;
-	DMAC[(DMAC_DMATCR1_addr&0xFF)>>2].data32=&DMAC_DMATCR[1];
-
-	//DMAC CHCR1 0xFFA0001C 0x1FA0001C 32 0x00000000 0x00000000 Held Held Bclk
-	DMAC[(DMAC_CHCR1_addr&0xFF)>>2].flags=REG_32BIT_READWRITE | REG_READ_DATA;
-	DMAC[(DMAC_CHCR1_addr&0xFF)>>2].readFunction=0;
-	DMAC[(DMAC_CHCR1_addr&0xFF)>>2].writeFunction=WriteCHCR<1>;
-	DMAC[(DMAC_CHCR1_addr&0xFF)>>2].data32=&DMAC_CHCR[1].full;
-
-	//DMAC SAR2 0xFFA00020 0x1FA00020 32 Undefined Undefined Held Held Bclk
-	DMAC[(DMAC_SAR2_addr&0xFF)>>2].flags=REG_32BIT_READWRITE | REG_READ_DATA | REG_WRITE_DATA;
-	DMAC[(DMAC_SAR2_addr&0xFF)>>2].readFunction=0;
-	DMAC[(DMAC_SAR2_addr&0xFF)>>2].writeFunction=0;
-	DMAC[(DMAC_SAR2_addr&0xFF)>>2].data32=&DMAC_SAR[2];
-
-	//DMAC DAR2 0xFFA00024 0x1FA00024 32 Undefined Undefined Held Held Bclk
-	DMAC[(DMAC_DAR2_addr&0xFF)>>2].flags=REG_32BIT_READWRITE | REG_READ_DATA | REG_WRITE_DATA;
-	DMAC[(DMAC_DAR2_addr&0xFF)>>2].readFunction=0;
-	DMAC[(DMAC_DAR2_addr&0xFF)>>2].writeFunction=0;
-	DMAC[(DMAC_DAR2_addr&0xFF)>>2].data32=&DMAC_DAR[2];
-
-	//DMAC DMATCR2 0xFFA00028 0x1FA00028 32 Undefined Undefined Held Held Bclk
-	DMAC[(DMAC_DMATCR2_addr&0xFF)>>2].flags=REG_32BIT_READWRITE | REG_READ_DATA | REG_WRITE_DATA;
-	DMAC[(DMAC_DMATCR2_addr&0xFF)>>2].readFunction=0;
-	DMAC[(DMAC_DMATCR2_addr&0xFF)>>2].writeFunction=0;
-	DMAC[(DMAC_DMATCR2_addr&0xFF)>>2].data32=&DMAC_DMATCR[2];
-
-	//DMAC CHCR2 0xFFA0002C 0x1FA0002C 32 0x00000000 0x00000000 Held Held Bclk
-	DMAC[(DMAC_CHCR2_addr&0xFF)>>2].flags=REG_32BIT_READWRITE | REG_READ_DATA;
-	DMAC[(DMAC_CHCR2_addr&0xFF)>>2].readFunction=0;
-	DMAC[(DMAC_CHCR2_addr&0xFF)>>2].writeFunction=WriteCHCR<2>;
-	DMAC[(DMAC_CHCR2_addr&0xFF)>>2].data32=&DMAC_CHCR[2].full;
-
-	//DMAC SAR3 0xFFA00030 0x1FA00030 32 Undefined Undefined Held Held Bclk
-	DMAC[(DMAC_SAR3_addr&0xFF)>>2].flags=REG_32BIT_READWRITE | REG_READ_DATA | REG_WRITE_DATA;
-	DMAC[(DMAC_SAR3_addr&0xFF)>>2].readFunction=0;
-	DMAC[(DMAC_SAR3_addr&0xFF)>>2].writeFunction=0;
-	DMAC[(DMAC_SAR3_addr&0xFF)>>2].data32=&DMAC_SAR[3];
-
-	//DMAC DAR3 0xFFA00034 0x1FA00034 32 Undefined Undefined Held Held Bclk
-	DMAC[(DMAC_DAR3_addr&0xFF)>>2].flags=REG_32BIT_READWRITE | REG_READ_DATA | REG_WRITE_DATA;
-	DMAC[(DMAC_DAR3_addr&0xFF)>>2].readFunction=0;
-	DMAC[(DMAC_DAR3_addr&0xFF)>>2].writeFunction=0;
-	DMAC[(DMAC_DAR3_addr&0xFF)>>2].data32=&DMAC_DAR[3];
-
-	//DMAC DMATCR3 0xFFA00038 0x1FA00038 32 Undefined Undefined Held Held Bclk
-	DMAC[(DMAC_DMATCR3_addr&0xFF)>>2].flags=REG_32BIT_READWRITE | REG_READ_DATA | REG_WRITE_DATA;
-	DMAC[(DMAC_DMATCR3_addr&0xFF)>>2].readFunction=0;
-	DMAC[(DMAC_DMATCR3_addr&0xFF)>>2].writeFunction=0;
-	DMAC[(DMAC_DMATCR3_addr&0xFF)>>2].data32=&DMAC_DMATCR[3];
-
-	//DMAC CHCR3 0xFFA0003C 0x1FA0003C 32 0x00000000 0x00000000 Held Held Bclk
-	DMAC[(DMAC_CHCR3_addr&0xFF)>>2].flags=REG_32BIT_READWRITE | REG_READ_DATA ;
-	DMAC[(DMAC_CHCR3_addr&0xFF)>>2].readFunction=0;
-	DMAC[(DMAC_CHCR3_addr&0xFF)>>2].writeFunction=WriteCHCR<3>;
-	DMAC[(DMAC_CHCR3_addr&0xFF)>>2].data32=&DMAC_CHCR[3].full;
-
-	//DMAC DMAOR 0xFFA00040 0x1FA00040 32 0x00000000 0x00000000 Held Held Bclk
-	DMAC[(DMAC_DMAOR_addr&0xFF)>>2].flags=REG_32BIT_READWRITE | REG_READ_DATA ;
-	DMAC[(DMAC_DMAOR_addr&0xFF)>>2].readFunction=0;
-	DMAC[(DMAC_DMAOR_addr&0xFF)>>2].writeFunction=WriteDMAOR;
-	DMAC[(DMAC_DMAOR_addr&0xFF)>>2].data32=&DMAC_DMAOR.full;
+	// DMAOR (global DMA operation register)
+	DMAC[((DMAC_DMAOR_addr) & 0xFF) >> 2].flags         = REG_32BIT_READWRITE | REG_READ_DATA;
+	DMAC[((DMAC_DMAOR_addr) & 0xFF) >> 2].readFunction  = 0;
+	DMAC[((DMAC_DMAOR_addr) & 0xFF) >> 2].writeFunction = WriteDMAOR;
+	DMAC[((DMAC_DMAOR_addr) & 0xFF) >> 2].data32        = &DMAC_DMAOR.full;
 }
+
+#undef DMAC_REG_RW
+#undef DMAC_REG_CHCR
+
+// ---------------------------------------------------------------------------
+// dmac_Reset - reset all channel registers to power-on state
+// ---------------------------------------------------------------------------
 void dmac_Reset(bool Manual)
 {
-	/*
-	DMAC SAR0 H'FFA0 0000 H'1FA0 0000 32 Undefined Undefined Held Held Bclk
-	DMAC DAR0 H'FFA0 0004 H'1FA0 0004 32 Undefined Undefined Held Held Bclk
-	DMAC DMATCR0 H'FFA0 0008 H'1FA0 0008 32 Undefined Undefined Held Held Bclk
-	DMAC CHCR0 H'FFA0 000C H'1FA0 000C 32 H'0000 0000 H'0000 0000 Held Held Bclk
-	DMAC SAR1 H'FFA0 0010 H'1FA0 0010 32 Undefined Undefined Held Held Bclk
-	DMAC DAR1 H'FFA0 0014 H'1FA0 0014 32 Undefined Undefined Held Held Bclk
-	DMAC DMATCR1 H'FFA0 0018 H'1FA0 0018 32 Undefined Undefined Held Held Bclk
-	DMAC CHCR1 H'FFA0 001C H'1FA0 001C 32 H'0000 0000 H'0000 0000 Held Held Bclk
-	DMAC SAR2 H'FFA0 0020 H'1FA0 0020 32 Undefined Undefined Held Held Bclk
-	DMAC DAR2 H'FFA0 0024 H'1FA0 0024 32 Undefined Undefined Held Held Bclk
-	DMAC DMATCR2 H'FFA0 0028 H'1FA0 0028 32 Undefined Undefined Held Held Bclk
-	DMAC CHCR2 H'FFA0 002C H'1FA0 002C 32 H'0000 0000 H'0000 0000 Held Held Bclk
-	DMAC SAR3 H'FFA0 0030 H'1FA0 0030 32 Undefined Undefined Held Held Bclk
-	DMAC DAR3 H'FFA0 0034 H'1FA0 0034 32 Undefined Undefined Held Held Bclk
-	DMAC DMATCR3 H'FFA0 0038 H'1FA0 0038 32 Undefined Undefined Held Held Bclk
-	DMAC CHCR3 H'FFA0 003C H'1FA0 003C 32 H'0000 0000 H'0000 0000 Held Held Bclk
-	DMAC DMAOR H'FFA0 0040 H'1FA0 0040 32 H'0000 0000 H'0000 0000 Held Held Bclk
-	*/
-	DMAC_CHCR[0].full = 0x0;
-	DMAC_CHCR[1].full = 0x0;
-	DMAC_CHCR[2].full = 0x0;
-	DMAC_CHCR[3].full = 0x0;
-	DMAC_DMAOR.full = 0x0;
+	for (int ch = 0; ch < 4; ch++)
+	{
+		// SAR/DAR/DMATCR are "undefined" after reset per SH4 manual — leave them
+		DMAC_CHCR[ch].full = 0x00000000u;
+	}
+	DMAC_DMAOR.full = 0x00000000u;
 }
+
+// ---------------------------------------------------------------------------
+// dmac_Term - nothing to release currently
+// ---------------------------------------------------------------------------
 void dmac_Term()
 {
 }
--- dc/sh4/dmac.h
+++ dc/sh4/dmac.h
@@ -1,143 +1,153 @@
 #pragma once
+// dmac.h - SH4 DMAC register types and public interface
+// Supports both little-endian (x86) and big-endian (Wii/PPC) hosts via
+// HOST_ENDIAN conditional bit-field ordering.
 
 #include "types.h"
 
+// ---------------------------------------------------------------------------
+// DMAC_CHCR_type - Channel Control Register (one per channel, 4 channels)
+// Reset value: 0x00000000
+// ---------------------------------------------------------------------------
 union DMAC_CHCR_type
 {
 	struct
 	{
-#if HOST_ENDIAN==ENDIAN_LITTLE
-		u32 DE:1;	//Channel Enable
-		u32 TE:1;	//Transfer End
-		u32 IE:1;	//Interrupt Enable
-		u32 res0:1;
+#if HOST_ENDIAN == ENDIAN_LITTLE
+		u32 DE   : 1;   // [0]     Channel Enable
+		u32 TE   : 1;   // [1]     Transfer End (set by hardware when done)
+		u32 IE   : 1;   // [2]     Interrupt Enable
+		u32 res0 : 1;   // [3]     reserved
 
-		u32 TS:3;	//Transmit Size
-		//u32 TS1:1;
-		//u32 TS2:1;
-		u32 TM:1;	//Transmit Mode
+		u32 TS   : 3;   // [6:4]   Transfer (word) Size: 000=8b 001=16b 010=32b 011=32bx4 100=32bx8
+		u32 TM   : 1;   // [7]     Transfer Mode: 0=cycle-steal, 1=burst
 
-		u32 RS:4;	//Resource Select
-		//u32 RS1:1;
-		//u32 RS2:1;
-		//u32 RS3:1;
+		u32 RS   : 4;   // [11:8]  Resource Select (DMA request source)
 
-		u32 SM:2;	//SRC mode
-		//u32 SM1:1;
-		u32 DM:2;	//DST mode
-		//u32 DM1:1;
+		u32 SM   : 2;   // [13:12] Source address Mode: 00=fixed 10=increment 11=decrement
+		u32 DM   : 2;   // [15:14] Destination address Mode: same encoding as SM
 
-		u32 AL:1;	//Acknowledge Level
-		u32 AM:1;	//Acknowledge Mode
-		u32 RL:1;	//In normal DMA mode, this bit is valid only in CHCR0 and CHCR1. In DDT mode, this bit is invalid.
-		u32 DS:1;	//In normal DMA mode, this bit is valid only in CHCR0 and CHCR1. In DDT mode, it is valid in CHCR0–CHCR3.
+		u32 AL   : 1;   // [16]    Acknowledge Level
+		u32 AM   : 1;   // [17]    Acknowledge Mode
+		u32 RL   : 1;   // [18]    Request Level (CHCR0/1 only in normal mode)
+		u32 DS   : 1;   // [19]    DREQ Select (CHCR0/1 normal; CHCR0-3 DDT)
 
-		u32 res1:4;
+		u32 res1 : 4;   // [23:20] reserved
 
-		u32 DTC:1;
-		u32 DSA:3;
-		//u32 DSA1:1;
-		//u32 DSA2:1;
+		u32 DTC  : 1;   // [24]    Destination Address Transfer Counter enable
+		u32 DSA  : 3;   // [27:25] Destination Space Attribute
 
-		u32 STC:1;
-		u32 SSA:3;
-		//u32 SSA1:1;
-		//u32 SSA2:1;
-#else
+		u32 STC  : 1;   // [28]    Source Address Transfer Counter enable
+		u32 SSA  : 3;   // [31:29] Source Space Attribute
+#else   // big-endian (Wii / PowerPC)
+		u32 SSA  : 3;
+		u32 STC  : 1;
 
-		u32 SSA:3;
-		u32 STC:1;
+		u32 DSA  : 3;
+		u32 DTC  : 1;
 
-		u32 DSA:3;
-		u32 DTC:1;
+		u32 res1 : 4;
 
-		u32 res1:4;
+		u32 DS   : 1;
+		u32 RL   : 1;
+		u32 AM   : 1;
+		u32 AL   : 1;
 
-		u32 DS:1;	//In normal DMA mode, this bit is valid only in CHCR0 and CHCR1. In DDT mode, it is valid in CHCR0–CHCR3.
-		u32 RL:1;	//In normal DMA mode, this bit is valid only in CHCR0 and CHCR1. In DDT mode, this bit is invalid.
-		u32 AM:1;	//Acknowledge Mode
-		u32 AL:1;	//Acknowledge Level
+		u32 DM   : 2;
+		u32 SM   : 2;
 
-		u32 DM:2;	//DST mode
-		u32 SM:2;	//SRC mode
+		u32 RS   : 4;
 
-		u32 RS:4;	//Resource Select
+		u32 TM   : 1;
+		u32 TS   : 3;
 
-		u32 TM:1;	//Transmit Mode
-		u32 TS:3;	//Transmit Size
-
-		u32 res0:1;
-		u32 IE:1;	//Interrupt Enable
-		u32 TE:1;	//Transfer End
-		u32 DE:1;	//Channel Enable
-
+		u32 res0 : 1;
+		u32 IE   : 1;
+		u32 TE   : 1;
+		u32 DE   : 1;
 #endif
 	};
 	u32 full;
 };
 
+// ---------------------------------------------------------------------------
+// DMAC_DMAOR_type - DMA Operation Register (global, shared by all channels)
+// Reset value: 0x00000000
+// ---------------------------------------------------------------------------
 union DMAC_DMAOR_type
 {
 	struct
 	{
-#if HOST_ENDIAN==ENDIAN_LITTLE
-		u32 DME:1;
-		u32 NMIF:1;
-		u32 AE:1;
-		u32 res0:1;
+#if HOST_ENDIAN == ENDIAN_LITTLE
+		u32 DME  : 1;   // [0]     DMA Master Enable (0=all channels disabled)
+		u32 NMIF : 1;   // [1]     NMI Flag (set on NMI while DMA active; write 0 to clear)
+		u32 AE   : 1;   // [2]     Address Error flag (set on address error; write 0 to clear)
+		u32 res0 : 1;   // [3]     reserved
 
-		u32 COD:1;
-		u32 res1:3;
-		
-		u32 PR0:1;
-		u32 PR1:1;
-		u32 res2:2;
+		u32 COD  : 1;   // [4]     Channel On Demand (used in DDT mode)
+		u32 res1 : 3;   // [7:5]   reserved
 
-		u32 res3:3;
-		u32 DDT:1;
+		u32 PR0  : 1;   // [8]     Priority: 00=round-robin, 01=ch0>1>2>3, 10=ch2>0>1>3
+		u32 PR1  : 1;   // [9]
+		u32 res2 : 2;   // [11:10] reserved
 
-		u32 res4:16;
+		u32 res3 : 3;   // [14:12] reserved
+		u32 DDT  : 1;   // [15]    DDT (On-Demand Data Transfer) mode enable
+
+		u32 res4 : 16;  // [31:16] reserved
 #else
+		u32 res4 : 16;
 
-		u32 res4:16;
+		u32 DDT  : 1;
+		u32 res3 : 3;
 
-		u32 DDT:1;
-		u32 res3:3;
+		u32 res2 : 2;
+		u32 PR1  : 1;
+		u32 PR0  : 1;
 
-		u32 res2:2;
-		u32 PR1:1;
-		u32 PR0:1;
+		u32 res1 : 3;
+		u32 COD  : 1;
 
-		u32 res1:3;
-		u32 COD:1;
-
-		u32 res0:1;
-		u32 AE:1;
-		u32 NMIF:1;
-		u32 DME:1;
+		u32 res0 : 1;
+		u32 AE   : 1;
+		u32 NMIF : 1;
+		u32 DME  : 1;
 #endif
 	};
 	u32 full;
 };
 
-extern u32 DMAC_SAR[4];
-extern u32 DMAC_DAR[4];
-extern u32 DMAC_DMATCR[4];//only 24 bits valid
-extern DMAC_CHCR_type DMAC_CHCR[4];
-
+// ---------------------------------------------------------------------------
+// DMAC register arrays (defined in dmac.cpp)
+// ---------------------------------------------------------------------------
+extern u32             DMAC_SAR[4];
+extern u32             DMAC_DAR[4];
+extern u32             DMAC_DMATCR[4];   // lower 24 bits valid per SH4 manual
+extern DMAC_CHCR_type  DMAC_CHCR[4];
 extern DMAC_DMAOR_type DMAC_DMAOR;
 
-//
-void dmac_ddt_ch0_ddt(u32 src,u32 dst,u32 count);
-void dmac_ddt_ch2_direct(u32 dst,u32 count);
+// ---------------------------------------------------------------------------
+// DMAC_DMAOR valid-bits mask
+// Bits that matter when checking DMAOR for a valid Ch2-DMA trigger:
+//   bit 15 (DDT=1), bit 3 (DME=1), and no error flags
+// ---------------------------------------------------------------------------
+#define DMAOR_MASK  0xFFFF8201u
+
+// ---------------------------------------------------------------------------
+// Public API
+// ---------------------------------------------------------------------------
+
+// On-demand / direct transfer (DDT mode) â€” stubs, not yet implemented
+void dmac_ddt_ch0_ddt(u32 src, u32 dst, u32 count);
+void dmac_ddt_ch2_direct(u32 dst, u32 count);
+
+// Trigger a Ch2 (PVR/TA) DMA transfer â€” called when SB_C2DST is written
 void DMAC_Ch2St();
 
-//Init/Res/Term
-void dmac_Init();
-void dmac_Reset(bool Manual);
-void dmac_Term();
+// Lifecycle
+void dmac_Init();               // register MMIO handlers
+void dmac_Reset(bool Manual);   // reset registers to power-on state
+void dmac_Term();               // teardown (currently a no-op)
+
+// Cycle-accurate DMA tick (called from the main emulation loop, not yet implemented)
 void UpdateDMA();
-
-
-
-#define DMAOR_MASK	0xFFFF8201
