8214f988c4cb68561e7cd6d617f7f65c29dbf9b2
BenoitAdam94 <adam050986@yahoo.fr>
2026-02-28 10:24:02 +0100

    sci: improve comments and suppress unused param warning

--- dc/sh4/sci.cpp
+++ dc/sh4/sci.cpp
@@ -1,18 +1,37 @@
-/*
-	Wasn't used on the dreamcast
-*/
+/**
+ * sci.cpp - Serial Communication Interface (SH4)
+ *
+ * The SCI peripheral was not used on the Dreamcast. All functions
+ * are intentional no-ops to satisfy the emulator lifecycle.
+ *
+ * SCI vs SCIF: The SH4 has two serial interfaces:
+ *   - SCI  (Serial Communication Interface)     -- unused on DC
+ *   - SCIF (Serial Communication Interface FIFO) -- used for debugging
+ */
+
 #include "types.h"
 #include "dc/mem/sh4_internal_reg.h"
 #include "sci.h"
 
+// Suppress unused parameter warnings for intentional no-op stubs
+#ifdef _MSC_VER
+#  define SCI_UNUSED(x) (void)(x)
+#else
+#  define SCI_UNUSED(x) (void)(x)
+#endif
 
-//Init term res
 void sci_Init()
 {
+	// SCI was not used on the Dreamcast -- nothing to initialize
 }
+
 void sci_Reset(bool Manual)
 {
+	// SCI was not used on the Dreamcast -- nothing to reset
+	SCI_UNUSED(Manual);
 }
+
 void sci_Term()
 {
+	// SCI was not used on the Dreamcast -- nothing to clean up
 }
--- dc/sh4/sci.h
+++ dc/sh4/sci.h
@@ -2,7 +2,14 @@
 
 #include "types.h"
 
-//Init/Res/Term
+/**
+ * SCI - Serial Communication Interface (SH4)
+ *
+ * NOTE: The SCI peripheral was not used by the Dreamcast.
+ * These stubs exist to satisfy the emulator's standard
+ * Init/Reset/Term lifecycle without performing any action.
+ */
+
 void sci_Init();
 void sci_Reset(bool Manual);
 void sci_Term();
